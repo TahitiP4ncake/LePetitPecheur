f2b2fda41a39772b60c4a6ece5cade188f9831b7
Baptiste <b.billet@rubika-edu.com>
2017-10-07 18:55:40 +0200

    dateTIME

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour {
 
@@ -10,6 +11,13 @@ public class GameManager : MonoBehaviour {
 
     private List<string> messages = new List<string>();
 
+    
+
+    DateTime lastDate;
+
+    DateTime lastOpening;
+
+
 	// Use this for initialization
 	void Start () {
 
@@ -17,17 +25,70 @@ public class GameManager : MonoBehaviour {
 
         messages = CSVReader.Read("Messages");
 
+        GetLastDate();
+
+        PlayerPrefs.Save();
+
+        DeterminePossibility();
+
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-		if ( Input.GetKeyDown(KeyCode.Escape) )
-		{
-			Application.Quit();
-		}
-}
+    void GetLastDate()
+    {
+        int _year = PlayerPrefs.GetInt("Year", -1);
+
+        if(_year == -1)
+        {
+            lastDate = DateTime.Now;
+            return;
+        }
+
+        int _month = PlayerPrefs.GetInt("Month", -1);
+        int _day = PlayerPrefs.GetInt("Day", -1);
+        int _hours = PlayerPrefs.GetInt("Hours", -1);
+        int _minutes = PlayerPrefs.GetInt("Minutes", -1);
+        int _secondes = PlayerPrefs.GetInt("Secondes", -1);
+
+        lastDate = new DateTime();
+        lastDate = lastDate.AddYears(_year-1);
+        lastDate = lastDate.AddMonths(_month-1);
+        lastDate = lastDate.AddDays(_day-1);
+        lastDate = lastDate.AddHours(_hours);
+        lastDate = lastDate.AddMinutes(_minutes);
+        lastDate = lastDate.AddSeconds(_secondes);
+
+    }
+
+    TimeSpan CalculInterval(DateTime _old, DateTime _now)
+    {
+        TimeSpan _ts = _now - _old;
+        return _ts;
+    }
+
+    void DeterminePossibility()
+    {
+        CalculInterval(lastDate, DateTime.Now);
+
+
+    }
 
 
 
+
+
+
+    void OnApplicationQuit()
+    {
+        SaveDayTime();
+    }
+
+    void SaveDayTime()
+    {
+        PlayerPrefs.SetInt("Year", DateTime.Now.Year);
+        PlayerPrefs.SetInt("Month", DateTime.Now.Month);
+        PlayerPrefs.SetInt("Day", DateTime.Now.Day);
+        PlayerPrefs.SetInt("Hours", DateTime.Now.Hour);
+        PlayerPrefs.SetInt("Minutes", DateTime.Now.Minute);
+        PlayerPrefs.SetInt("Secondes", DateTime.Now.Second);
+    }
 }
