a6bc29952ac9405c364616a47a86630f8c718463
TahitiP4ncake <dorian.pancake@gmail.com>
2017-11-02 16:47:26 +0100

    animations du menu

---
+++ Assets/Scripts/MouseOver.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+public class MouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler// required interface when using the OnPointerEnter method.
+{
+
+    public MenuAnimation menu;
+
+    public string actionSelected;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (actionSelected != "Quit" && actionSelected != "Confirmation" && actionSelected!="Menu")
+            menu.Show(actionSelected);
+        //Debug.Log("The cursor entered the selectable UI element.");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+        menu.Hide(actionSelected);
+        //Debug.Log("The cursor exited the selectable UI element.");
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(actionSelected =="Quit" || actionSelected=="Menu")
+        menu.Show(actionSelected);
+
+        if (actionSelected == "Confirmation")
+            menu.Quit();
+        //Debug.Log("The cursor clicked the selectable UI element.");
+    }
+
+}
