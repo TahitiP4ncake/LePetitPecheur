1f55bf241adf680cd089eb3d5fcd712b9b70d64e
Baptiste <mail@baptistebillet.com>
2017-11-20 23:15:34 +0100

    Radio

---
+++ Assets/Scripts/Radio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Song
+{
+
+}
+
+public class Radio : MonoBehaviour {
+
+    //Radio Kill the Video Star
+
+	void Play()
+    {
+
+    }
+
+    void Next()
+    {
+
+    }
+
+    void Previous()
+    {
+
+    }
+
+    void AddSong()
+    {
+
+    }
+
+}
