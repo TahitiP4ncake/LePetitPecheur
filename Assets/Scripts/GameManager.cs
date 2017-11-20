7bd8077bb56e398b3af7158299283573f8ceb563
Baptiste <mail@baptistebillet.com>
2017-11-20 23:42:41 +0100

    Radio kill the video star

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
@@ -11,41 +11,17 @@ public class GameManager : MonoBehaviour {
 
 	private Vector2 lastPosition;
 
-<<<<<<< HEAD
-    private List<string> allMessages = new List<string>();
-
-    private List<string> randomMessages = new List<string>();
-=======
     // All the messages in the .CSV
     public List<string> allMessages = new List<string>();
 
     // List of all messages not displayed yet, randomized
     public List<string> randomMessages = new List<string>();
->>>>>>> 6ff15692e9773537af8851b5ad3eb5bce71520f5
 
     DateTime lastDate;
 
     DateTime lastOpening;
 
-<<<<<<< HEAD
-    DateTime timerMessage;
-
-    DateTime timerObject;
-
-    bool canHaveMessage = false;
-    bool canHaveObject = false;
-
-    int dayBeforeMessage = 1;
-    int minutesBeforeObject;
-
-    bool isBusy = false;
-
-    int nbrOfMessages;
-
-
-=======
     int timerObject;
->>>>>>> 6ff15692e9773537af8851b5ad3eb5bce71520f5
 
     int timerMessage;
 
@@ -71,22 +47,11 @@ public class GameManager : MonoBehaviour {
         // System settings
 		Application.runInBackground = true;
 
-<<<<<<< HEAD
-        allMessages = CSVReader.Read("Messages");
-
-        GetSavedData();
-
-        if(allMessages.Count != nbrOfMessages)
-        {
-            ActualizeMessagesList();
-        }
-=======
         // Get all the messages from the .CSV
         allMessages = CSVReader.Read("Messages");
 
         // Get all the old data
         GetSavedData();
->>>>>>> 6ff15692e9773537af8851b5ad3eb5bce71520f5
 
         // If the .CSV change since the last time (aka new messages)
         if(allMessages.Count != nbrOfMessages)
@@ -97,16 +62,6 @@ public class GameManager : MonoBehaviour {
         // Save the actual settings
         PlayerPrefs.Save();
 
-<<<<<<< HEAD
-        if (canHaveMessage == false)
-        {
-            DeterminePossibility();
-        }
-
-        StartCoroutine(TimePassed());
-
-        minutesBeforeObject = UnityEngine.Random.Range(20, 31);
-=======
         // If the player can't have new message the last time, determinePossibilities to show the time spend out of the game
         if (canHaveMessage == false)
         {
@@ -123,31 +78,21 @@ public class GameManager : MonoBehaviour {
 
         // Define minutes before new objects
         minutesBeforeObject = UnityEngine.Random.Range(20,32);
->>>>>>> 6ff15692e9773537af8851b5ad3eb5bce71520f5
 
     }
 
     void GetSavedData()
     {
         // Messages
-<<<<<<< HEAD
-        int _nbrMessages = PlayerPrefs.GetInt("NbrMessages", 0);
-
-        for (int i = 0; i < _nbrMessages; i++)
-=======
         int _nbrRandomMessages = PlayerPrefs.GetInt("RandomMessages", 0);
 
         for (int i = 0; i < _nbrRandomMessages; i++)
->>>>>>> 6ff15692e9773537af8851b5ad3eb5bce71520f5
         {
             randomMessages.Add(PlayerPrefs.GetString("RandomMessages" + i));
         }
 
-<<<<<<< HEAD
-=======
         nbrOfMessages = PlayerPrefs.GetInt("NbrMessages", 0);
 
->>>>>>> 6ff15692e9773537af8851b5ad3eb5bce71520f5
         // Date
 
         int _year = PlayerPrefs.GetInt("Year", -1);
@@ -173,19 +118,6 @@ public class GameManager : MonoBehaviour {
         lastDate = lastDate.AddSeconds(_secondes);
 
         // Fishing Time
-<<<<<<< HEAD
-        int _ft = PlayerPrefs.GetInt("TimerMessage", 0);
-        timerMessage.AddMinutes(_ft);
-
-        _ft = PlayerPrefs.GetInt("TimerObject", 0);
-        timerObject.AddMinutes(_ft);
-
-
-        // Can have
-
-        int _intToBool;
-        _intToBool = PlayerPrefs.GetInt("CanHaveObject", 0);
-=======
         int memoryValue = PlayerPrefs.GetInt("TimerMessage", 0);
         timerMessage += memoryValue;
 
@@ -197,7 +129,6 @@ public class GameManager : MonoBehaviour {
 
         int _intToBool;
         _intToBool = PlayerPrefs.GetInt("CanHaveMessage", 0);
->>>>>>> 6ff15692e9773537af8851b5ad3eb5bce71520f5
 
         if(_intToBool==0)
         {
@@ -219,114 +150,6 @@ public class GameManager : MonoBehaviour {
             canHaveObject = true;
         }
 
-<<<<<<< HEAD
-    }
-
-    void DeterminePossibility()
-    {
-        TimeSpan _ts = DateTime.Now - lastDate;
-
-        if(_ts.TotalDays >1)
-        {
-            canHaveMessage = true;
-        }
-    }
-
-    IEnumerator TimePassed()
-    {
-        while(true)
-        {
-            yield return new WaitForSeconds(1);
-            timerMessage.AddSeconds(1);
-            timerObject.AddSeconds(1);
-            
-            if(timerMessage.Day>= dayBeforeMessage)
-            {
-                timerMessage = new DateTime();
-                canHaveMessage = true;
-            }
-
-            if(timerObject.Minute>= minutesBeforeObject)
-            {
-                timerObject = new DateTime();
-                canHaveObject = true;
-            }
-
-
-            if (isBusy == false)
-            {
-                if (canHaveMessage)
-                {
-                    isBusy = true;
-
-                    GetNewMessage();
-                }
-
-                if (canHaveObject)
-                {
-                    isBusy = true;
-
-                    GetNewObject();
-                }
-            }
-        }
-    }
-
-    void GetNewMessage()
-    {
-
-    }
-
-    void GetNewObject()
-    {
-
-    }
-
-    void ActualizeMessagesList()
-    {
-        if (allMessages.Count < nbrOfMessages)
-        {
-            ResetMessages();
-        }
-        else
-        if (allMessages.Count > nbrOfMessages)
-        {
-            // Add new messages to the possibilities
-
-            List<string> _newMessages = new List<string>();
-
-            for(int i = nbrOfMessages+1; i<allMessages.Count; i++)
-            {
-                _newMessages.Add(allMessages[i]);
-            }
-
-        }
-
-        nbrOfMessages = randomMessages.Count;
-
-    }
-
-    // Fill the randomMessages with the message list, randomize
-    void ResetMessages()
-    {
-        randomMessages = new List<string>();
-
-        List<String> _temporaryList = new List<string>();
-
-        foreach(String _string in allMessages)
-        {
-            _temporaryList.Add(_string);
-        }
-
-        int _index;
-
-        for(int i = 0; i< _temporaryList.Count; i++)
-        {
-            _index = UnityEngine.Random.Range(0, _temporaryList.Count + 1);
-
-            randomMessages.Add(_temporaryList[_index]);
-
-=======
     }
 
     void DeterminePossibilities()
@@ -492,7 +315,6 @@ public class GameManager : MonoBehaviour {
 
             randomMessages.Add(_temporaryList[_index]);
 
->>>>>>> 6ff15692e9773537af8851b5ad3eb5bce71520f5
             _temporaryList.RemoveAt(_index);
 
         }
@@ -504,10 +326,6 @@ public class GameManager : MonoBehaviour {
         SaveData();
     }
 
-<<<<<<< HEAD
-    void SaveData()
-    {
-=======
     /// <summary>
     /// Save Data
     /// </summary>
@@ -517,7 +335,6 @@ public class GameManager : MonoBehaviour {
 
         PlayerPrefs.SetInt("NbrMessages", nbrOfMessages);
 
->>>>>>> 6ff15692e9773537af8851b5ad3eb5bce71520f5
         // Last date
         PlayerPrefs.SetInt("Year", DateTime.Now.Year);
         PlayerPrefs.SetInt("Month", DateTime.Now.Month);
@@ -527,16 +344,8 @@ public class GameManager : MonoBehaviour {
         PlayerPrefs.SetInt("Secondes", DateTime.Now.Second);
 
         // Timer fishing
-<<<<<<< HEAD
-        TimeSpan _ts; 
-        _ts = timerMessage.TimeOfDay;
-        PlayerPrefs.SetInt("TimerMessage", (int)_ts.TotalMinutes);
-        _ts = timerObject.TimeOfDay;
-        PlayerPrefs.SetInt("TimerObject", (int)_ts.TotalMinutes);
-=======
         PlayerPrefs.SetInt("TimerMessage", timerMessage);
         PlayerPrefs.SetInt("TimerObject", timerObject);
->>>>>>> 6ff15692e9773537af8851b5ad3eb5bce71520f5
 
         // Can Have
         if (canHaveMessage)
@@ -563,9 +372,6 @@ public class GameManager : MonoBehaviour {
         {
             PlayerPrefs.SetString("RandomMessages" + i, randomMessages[i]);
         }
-<<<<<<< HEAD
-        
-=======
 
         PlayerPrefs.SetInt("RandomMessages", randomMessages.Count);
 
@@ -587,7 +393,6 @@ public class GameManager : MonoBehaviour {
         {
             return false;
         }
->>>>>>> 6ff15692e9773537af8851b5ad3eb5bce71520f5
     }
 
     bool CanHaveMessage(int _seconds)
--- Assets/Scripts/Radio.cs
+++ Assets/Scripts/Radio.cs
@@ -1,34 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public enum Song
-{
-
-}
+using System;
 
 public class Radio : MonoBehaviour {
+
+    public List<AudioClip> songs = new List<AudioClip>();
+    
+    AudioSource _audioSource;
+
+    void Start()
+    {
+        _audioSource = Harmony.SetSource(songs[1].ToString());
+
+        /*
+        Harmony.EnabledPlayList(_audioSource, true);
+
+        Harmony.AddClip(_audioSource, songs[1]);
+
+        Harmony.EnabledPlaylistLoop(_audioSource, true);
+        */
+
+    }
+
 
     //Radio Kill the Video Star
 
 	void Play()
     {
+        Harmony.Play(_audioSource, true);
+    }
 
+    void Pause()
+    {
+        Harmony.Pause(_audioSource);
+    }
+
+    void UnPause()
+    {
+        Harmony.UnPause(_audioSource);
+    }
+
+    void Stop()
+    {
+        Harmony.Stop(_audioSource);
     }
 
     void Next()
     {
-
+        Harmony.NextClip(_audioSource);
     }
 
     void Previous()
     {
-
-    }
-
-    void AddSong()
-    {
-
+        Harmony.PreviousClip(_audioSource);
     }
 
 }
