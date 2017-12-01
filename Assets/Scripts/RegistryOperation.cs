7ef861e1afa8d6b9eef1246cf478e0e8e3c7b28b
Baptiste <b.billet@rubika-edu.com>
2017-12-02 00:08:55 +0100

    Clean the open at start methods

---
+++ Assets/Scripts/RegistryOperation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Microsoft.Win32;
+using System.Windows.Forms;
+
+public class RegistryOperation : MonoBehaviour {
+
+    public bool openAtStart;
+
+    public void OpenApplicationAtStart()
+    {
+        RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+        rk.SetValue("Pêcheur.exe", System.Windows.Forms.Application.ExecutablePath.ToString());
+
+        openAtStart = true;
+    }
+
+    public void DontOpenApplicationAtStart()
+    {
+        RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+        rk.DeleteSubKey("Pêcheur.exe");
+
+        openAtStart = false;
+    }
+}
